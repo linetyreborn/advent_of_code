524cc7b470af13b20acac020d2f118ffe70c83ba
linetyreborn <o.hedda@gmail.com>
2023-12-04 00:14:54 +0100

    day 3 - code not working 100% , missing few cases

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -4,61 +4,128 @@ use std::fs;
 use std::fmt::format;
 use std::env;
 #[tokio::main]
-pub async fn run(){
+pub async fn run() {
+      // this method needs to be inside main() method
+    // env::set_var("RUST_BACKTRACE", "full");
+    let input_strings  = read_string_from_file("ressources/input3.txt");
+    
+    let input_strings = "
+    ...733.......289..262.....520..................161.462..........450.........................183..................100*.......................
+    ....*....................*.............707.352....*............/.....................801...@...............333..196........484.635......287*
+    ....42.........131....913..............*......&..........634..................440..&...............83.....@...........404$..=....*..423.*...
+    618.......272....*.........&......547.344...............#............689.589.*....150......382=................................168......433.";
+    
+     //*/
+    let mut full_indices1 = HashMap::new();
+    let mut size = 0;
 
-  // this method needs to be inside main() method
-  //env::set_var("RUST_BACKTRACE", "1");
-    
-    /*
-        let exercite_url = "https://adventofcode.com/2023/day/3";
-    */
-    let input  = read_string_from_file("ressources/input3.txt");
-    let mut part_numbers: Vec<String> = Vec::new();
-    // let mut result = 0;
-    // let mut games = Vec::new();
-    // let mut big_power = 0;
-    // Create a regex to match sequences of digits
-    
     let re = Regex::new(r"\d+").unwrap();
 
-    for line in input.lines() {
-        let new_line = line.to_string();
+    // let mut full_indices: Vec<(usize, Vec<usize>)> = Vec::new();
+    for (idx , input_string_input) in input_strings.lines().enumerate() {
+        let mut input_string = input_string_input.trim();
+        let new_line = input_string.to_string();
         let numbers: Vec<String> = re.find_iter(&new_line)
                                      .map(|mat| mat.as_str().to_string())
                                      .collect();
+        let mut indices = Vec::new();
+        for (index, character) in input_string.char_indices() {
+            if !character.is_digit(10) && character != '.' && character != '\n' && character != ' ' {
+                indices.push(index);
+                // println!("{:?} {:?} {:?}", index , input_string.chars().nth(index), input_string.chars().nth(index + 1));
+            }
+        }
+        // full_indices.push((idx, indices.clone()));
+        full_indices1.insert(idx,(input_string, indices, numbers));
+        size += 1;
+    }
+    for (idx , input_string) in input_strings.lines().enumerate() {
+        
+    }
+    let mut i: usize = 0;
+    let mut valid_part_numbers= HashMap::new();
+    
+    while i < size {
+        let mut line_i_minus_1 = (0, "", Vec::new(), Vec::new());
+        let mut line_i = (0, "", Vec::new(), Vec::new());
+        let mut line_i_plus_1 = (0, "", Vec::new(), Vec::new());
 
-        let mut indices: Vec<(String, Option<usize>)> = Vec::new();
-        let mut result = String::new();
-        println!("Numbers: {:?}", numbers);
-        for item in numbers.iter() {
-            let index = line.find(item);
-            indices.push((item.clone(), index));
-            if let Some(index) = index {
-                let start_idx = if index > 0 { index - 1 } else { index };
-                let lenght = if item.chars().count() < line.chars().count() { 1 } else { 0 };
-                let first_char = &get_substring(line, start_idx, item.chars().count()).chars().nth(0).unwrap();
-                let last_char = &get_substring(line, item.chars().count(), lenght).chars().nth(0).unwrap();
-                if item == "102" {
-                    println!("{}", last_char);
-                }
-                if check_is_symbol(*first_char) || check_is_symbol(*last_char) {
-                    result += &get_substring(line, index, item.chars().count());
+        if i > 0 {
+            if let Some((str_line, idx,numbers)) = full_indices1.get(&(i -1)) {
+                line_i_minus_1 = (i-1, str_line, idx.to_vec(), numbers.to_vec());
+            }
+        }
+        if let Some((str_line, idx , numbers)) = full_indices1.get(&(i)) {
+            line_i = (i, str_line, idx.to_vec(), numbers.to_vec());
+
+        }
+
+        if i < size - 1 {
+            if let Some((str_line, idx, numbers)) = full_indices1.get(&(i +1)) {
+                line_i_plus_1 = (i+1, str_line, idx.to_vec(), numbers.to_vec());
+                // println!("line {} : {:?} : {:?}", i, str_line,idx);
+            }
+        }
+         if line_i_minus_1.3.len() > 0 {
+            for num in line_i_minus_1.3.clone().into_iter(){
+                if let Some(num_pos) = line_i_minus_1.1.find(&num) {
+                    for sym_pos in line_i.2.iter() {
+                        if num_pos > 0 && &(num_pos - 1) <= sym_pos && sym_pos <= &(num_pos + num.len()) {
+                            valid_part_numbers.insert((line_i_minus_1.0, num_pos), num.clone());
+                        } else if num_pos == 0 && sym_pos <= &(num.len()) {
+                            valid_part_numbers.insert((line_i_minus_1.0, num_pos), num.clone());
+                        }
+                    }
                 }
             }
         }
 
-        println!("Result: {}", result);
+        for sym_pos in line_i_minus_1.2.iter() {
+            for num in line_i_minus_1.3.iter() {
+                if let Some(num_pos) = line_i_minus_1.1.find(&*num) {
+                    if num_pos > 0 && &(num_pos - 1) <= sym_pos && sym_pos <= &(num_pos + num.len()) {
+                        valid_part_numbers.insert((line_i_minus_1.0, num_pos), num.clone());
+                    } else if num_pos == 0 && sym_pos <= &(num.len()) {
+                        valid_part_numbers.insert((line_i_minus_1.0, num_pos), num.clone());
+                    }
+                }
+            }
+            for num in &line_i.3{
+                if let Some(num_pos) = line_i.1.find(&*num) {
+                    if num_pos > 0 && &(num_pos - 1) <= sym_pos && sym_pos <= &(num_pos + num.len()) {
+                        valid_part_numbers.insert((line_i.0, num_pos), num.clone());
+                    } else if num_pos == 0 && sym_pos <= &(num.len()) {
+                        valid_part_numbers.insert((line_i.0, num_pos), num.clone());
+                    }
+                }
+            }
+        }
+
+        for sym_pos in line_i.2.iter() {
+            for num in line_i.3.iter() {
+                if let Some(num_pos) = line_i.1.find(&*num) {
+                    if num_pos > 0 && &(num_pos - 1) <= sym_pos && sym_pos <= &(num_pos + num.len()) {
+                        valid_part_numbers.insert((line_i.0, num_pos), num.clone());
+                    } else if num_pos == 0 && sym_pos <= &(num.len()) {
+                        valid_part_numbers.insert((line_i.0, num_pos), num.clone());
+                    }
+                }
+            }
+        }
+        // println!("line_i {} : {:?}",i , line_i);
+        i += 1;
     }
+    // println!("valid_part_numbers : {:?}", valid_part_numbers);
+    let mut somme = 0;
+    for (k, v) in valid_part_numbers {
+        if let Ok(calibration) = v.parse::<i32>() {
+            somme += calibration;
+        }
+    }
+    println!("somme : {:?}", somme);   
+
 }
 
-fn check_is_symbol(c: char) -> bool {
-    !c.is_digit(10) && c != '.'
-}
-
-fn get_substring(input: &str, start: usize, length: usize) -> String {
-    let end_index = start + length;
-    return format!("{}, ", input[start..end_index].to_string())
-}
 
 fn read_string_from_file(path: &str) -> String {
     match fs::read_to_string(path) {
